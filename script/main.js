3858dbdb388f869a0a2771f6f2cd94731c9f367c
thewilddoor <156068311+thewilddoor@users.noreply.github.com>
2024-11-27 01:29:31 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -120,
+      y: 150,
       rotation: -180,
       opacity: 0
     })
